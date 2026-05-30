ff96b9fae037098a36bf151b0efbadfbb981ebf5
Lilian <lilianginga86@gmail.com>
2026-05-30 21:32:40 +0300

    Made UIs for both activites, and styles for 2 buttons and the searchbar

---
+++ app/src/main/java/com/recacer/pokedextracker/TrackerJSON.java
@@ -0,0 +1,21 @@
+package com.recacer.pokedextracker;
+
+public class TrackerJSON {
+
+
+    public static void createInternalJSON(){
+
+    }
+
+    public static void readInternalJSON(){
+
+    }
+
+    public static void writeExternalJSON(){
+
+    }
+
+    public static void readPokedexJSON(){
+
+    }
+}
